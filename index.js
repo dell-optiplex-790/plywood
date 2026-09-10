4320bda2916e9c4b4bb4c9210fd0168b0b7db74b
dell-optiplex-790 <dell.optiplex.d4719126@gmail.com>
2026-09-10 13:54:12 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
 (async function $r() {
-  try {
-    var fs = await import('node:fs');
-    var path = await import('node:path');
-  } catch(e) {return console.log('bad env', e)}
+  var fs = await import('node:fs');
+  var path = await import('node:path');
   var files = fs.readdirSync('..', {withFileTypes: true, recursive: true}).filter(e => e.isFile() && e.name.endsWith('.js')).map(e => path.join(e.parentPath, e.name));
   console.log(files);
   fs.writeFileSync('oops.txt', 'you ran something...');
